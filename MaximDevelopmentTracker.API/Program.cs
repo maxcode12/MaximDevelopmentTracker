3c6f5395df5705ba915464a2eb68de922a18d58b
Maxwell Aruakpor <aruakpo@uwindsor.ca>
2025-01-02 09:02:18 -0500

    added swagger

--- MaximDevelopmentTracker.API/Program.cs
+++ MaximDevelopmentTracker.API/Program.cs
@@ -14,14 +14,22 @@ builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", 
+        new() { Title = "MaximDevelopmentTracker.API", 
+            Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MaximDevelopmentTracker.API v1"));
 }
 
 app.UseHttpsRedirection();
